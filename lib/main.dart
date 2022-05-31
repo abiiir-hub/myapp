d1f1aa8fc1787edfac8b3a13096da24159f2c2de
Abiiir <ja_safta@esi.dz>
2022-05-31 21:55:55 +0200

    changed text color to blue

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordpair = WordPair.random();
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text('WordPair Generator')),
+          ),
+          body: Center(
+            child: Text(
+              wordpair.asPascalCase,
+              style: TextStyle(color: Colors.blue),
+            ),
+          )),
+    );
+  }
+}
